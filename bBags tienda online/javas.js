530a1bdc9d45fd6d07ac4e4128d17c1df4c4661c
bernialonso <144186335+bernialonso@users.noreply.github.com>
2023-09-05 21:30:11 -0300

    Add files via upload

---
+++ bBags tienda online/javas.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Add your JavaScript code here to handle form submission
+    // For example, you can use Ajax to send the form data to the server
+  });
